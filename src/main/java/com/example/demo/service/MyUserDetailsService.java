4f3a595491be35c29e0fee981c6ab322e124671d
Kranthi-45 <kranthikumarbolli@gmail.com>
2023-12-10 02:55:05 +0530

    correctin myuser table name as per db

--- src/main/java/com/example/demo/service/MyUserDetailsService.java
+++ src/main/java/com/example/demo/service/MyUserDetailsService.java
@@ -21,15 +21,15 @@ public class MyUserDetailsService {
 	private JdbcTemplate jdbcTemplate;
 
 	public int create(MyUser user) {
-		String sql="INSERT INTO MyUser VALUES(?,?,?,?,?,?,?)";
+		String sql="INSERT INTO myuser VALUES(?,?,?,?,?,?,?)";
 		return jdbcTemplate.update(sql, user.getUserId(), user.getUsername(),user.getPassword(), "user", user.getEmail(), user.getPhone(), user.getGender());
 	}
 	public List<MyUser> read() {
-		return jdbcTemplate.query("SELECT * FROM MyUser", new MyUserRowMapper());
+		return jdbcTemplate.query("SELECT * FROM myuser", new MyUserRowMapper());
 	}
 	public Optional<MyUser> read(String username) {
 		try {
-			MyUser user = jdbcTemplate.queryForObject("SELECT * FROM MyUser WHERE username=?", new MyUserRowMapper(), username);
+			MyUser user = jdbcTemplate.queryForObject("SELECT * FROM myuser WHERE username=?", new MyUserRowMapper(), username);
 			System.out.println("User in Service "+ user);
 			return Optional.of(user);
 		} catch (EmptyResultDataAccessException e) {
@@ -37,11 +37,11 @@ public class MyUserDetailsService {
 	    }
 	}
 	public int update(MyUser user) {
-		String sql="UPDATE MyUser SET username=?, email=?, phone=?, gender=?,  password=? WHERE userId=?";
+		String sql="UPDATE myuser SET username=?, email=?, phone=?, gender=?,  password=? WHERE userId=?";
 		return jdbcTemplate.update(sql, user.getUsername(),user.getEmail(), user.getPhone(), user.getGender(), user.getPassword(), user.getUserId());
 	}
 	public int delete(Long userId) {
-		String sql="DELETE FROM MyUser WHERE userId=?";
+		String sql="DELETE FROM myuser WHERE userId=?";
 		return jdbcTemplate.update(sql, userId);
 	}
 //	public int modifyUser(MyUser user) {
@@ -49,7 +49,7 @@ public class MyUserDetailsService {
 //		return jdbcTemplate.update(sql,user.getUsername(), user.getEmail(), user.getPhone(), user.getGender(), user.getPassword(), user.getRole(), user.getUserId());
 //	}
 	public MyUser modifyUser(MyUser user) {
-		String sql="UPDATE MyUser SET  username=?, email=?, phone=?, gender=? WHERE userId=? ";
+		String sql="UPDATE myuser SET  username=?, email=?, phone=?, gender=? WHERE userId=? ";
 		int success = jdbcTemplate.update(sql,user.getUsername(), user.getEmail(), user.getPhone(), user.getGender(), user.getUserId());
 		if(success == 1) {
 			return user;
@@ -58,20 +58,20 @@ public class MyUserDetailsService {
 		}
 	}
 	public int modifyUserbyUserName(MyUser user) {
-		String sql="UPDATE MyUser SET  username=?, email=?, phone=?, gender=? WHERE username=? ";
+		String sql="UPDATE myuser SET  username=?, email=?, phone=?, gender=? WHERE username=? ";
 		return jdbcTemplate.update(sql,user.getUsername(), user.getEmail(), user.getPhone(), user.getGender(), user.getUserId());
 	}
 	public int deleteUser(String username) {
-		String sql="DELETE FROM MyUser WHERE username=?";
+		String sql="DELETE FROM myuser WHERE username=?";
 		return jdbcTemplate.update(sql, username);
    
 	}
 	public MyUser getUserById(Long id) {
-		String sql = "SELECT * FROM MyUser where userId= ?";
+		String sql = "SELECT * FROM myuser where userId= ?";
 		return jdbcTemplate.queryForObject(sql, new MyUserRowMapper(),id);
 	}
 	public MyUser getUsersCount() {
-		String sql = "SELECT count(userId) FROM MyUser ";
+		String sql = "SELECT count(userId) FROM myuser ";
 		return jdbcTemplate.queryForObject(sql, new UsersCountRowMapper());
 
 	}
